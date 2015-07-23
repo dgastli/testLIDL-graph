daedf8edce85878b742cbd5cd704196c14aeea0a
dgastli <gastli.dorra@mail.com>
2015-07-23 11:58:19 +0200

    boites editables

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 // var CodeBlock = require('./CodeBlock.jsx');
 var CodeBlockViewer = require('./CodeBlockViewer.jsx');
 var React = require('react');
+var timer;
 
-
+/*
+var interactionToShow ={
+  type:"InteractionSimple",
+  operator:[],
+  operand:[]
+};*/
 
 var interactionToShow = {
   type:"InteractionSimple",
@@ -18,6 +24,38 @@ var interactionToShow = {
   }]
 };
 
+if(window.onload) {
+        var curronload = window.onload;
+        var newonload = function() {
+            curronload();
+            afterLoad();
+        };
+        window.onload = newonload;
+} else {
+        window.onload = afterLoad;
+}
+
+function maybeupdate() {
+  updateDelayPassed=false;
+  clearTimeout(timer);
+  timer=setTimeout(update,500);
+}
+
+function update() {
+  try{
+    var interactionToShow = Utils.fromString(document.getElementById("main").value);
+  }catch(error){
+
+  }
+}
+function afterLoad(){
+
+  document.getElementById('main').addEventListener('input',maybeupdate);
+
+  update();
+
+}
+
 
 
 class Main extends React.Component {
@@ -25,6 +63,8 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
   }
+
+
 
   render() {
     return (<CodeBlockViewer interaction={interactionToShow}/>);
